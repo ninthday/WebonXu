01152b5df2610f75b93b9b12e562360c9092c8d7
Jeffy Shih <jeffy@ninthday.info>
2013-11-25 15:17:19 +0800

    Delete setup.inc.php

--- inc/setup.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('_WEB_NAME','Fire and Flood Project | NCCU');                       //網站名稱
-define('_WEB_ADDR','http://140.119.163.198/~jeffy/WFonXuServer/');           //網站位址
-define('_APP_PATH', str_replace('inc','',str_replace('\\', '/', dirname(__FILE__))));   //定義網站實體位置
-?>
